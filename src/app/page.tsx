022aabbb53337e1ff521f7e18c55a63ff9234b9a
Dio Rizqi <124369676+diorizqi404@users.noreply.github.com>
2024-11-13 16:07:46 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
       </div>
       <h2 className={inter.className}>
-        tes
+        tesgggregre
       </h2>
 
       <div className={styles.center}>

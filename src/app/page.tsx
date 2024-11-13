a11a193b020dab470656c877a8e4ace168140a28
Dio Rizqi <124369676+diorizqi404@users.noreply.github.com>
2024-11-13 16:10:22 +0700

    tes teros sampe bisa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
       </div>
       <h2 className={inter.className}>
-        tesgggregre
+        tesgggregrelagisampebisa
       </h2>
 
       <div className={styles.center}>
